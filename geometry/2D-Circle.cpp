2b8c2f8c3534312b19d88fa30811a8cd9ddf1237
RangeLFR <RangeLFR@163.com>
2018-08-16 11:24:43 +0800

    add area

--- geometry/2D-Circle.cpp
+++ geometry/2D-Circle.cpp
@@ -89,3 +89,15 @@ int getTangents(Circle A, Circle B, Point* a, Point* b) {
     return cnt;
 }
 
+double Circle_Circle_Area_of_overlap(Circle c1, Circle c2){
+    double d=Length(c1.c-c2.c);
+    if(dcmp(c1.r+c2.r-d)<=0) return 0;
+    if(dcmp(fabs(c1.r-c2.r)-d)>=0){
+        double minr = min(c1.r,c2.r);
+        return PI*minr*minr;
+    }
+    double x=(d*d + c1.r*c1.r - c2.r*c2.r)/(2*d);
+    double t1=acos(x/c1.r);
+    double t2=acos((d-x)/c2.r);
+    return c1.r*c1.r*t1 + c2.r*c2.r*t2 - d*c1.r*sin(t1);
+}
